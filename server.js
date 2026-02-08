4aeef02fae85dd86d21af42cf97e16db576f784e
Daniel130me <kosokodaniel@gmail.com>
2026-02-08 01:48:44 +0100

    update for deployment

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const __dirname = path.dirname(__filename);
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: path.join(__dirname, "dist"),
+});
 
 const port = process.env.PORT || 5000;
 
 server.use(middlewares);
 
 server.use("/api", router);
-
-server.use(express.static(path.join(__dirname, "dist")));
 
 server.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
